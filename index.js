ce042e37e240451e23c1286a728756e687342916
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-11-09 11:35:35 +0000

    Add sharp for resize image

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import multer from "multer";
+import sharp from "sharp";
 const app = express();
 const upload = multer({ dest: "uploads/" });
 const PORT = process.env.PORT || 3000;
@@ -15,10 +16,14 @@ app.post("/test", (req, res) => {
   res.send({ msg: "Response to POST request to /" });
 });
 
-app.post("/image", upload.single("avatar"), function (req, res, next) {
-  res.send({ msg: "saved the image successfully" });
-  // req.file is the `avatar` file
-  // req.body will hold the text fields, if there were any
+app.post("/image", upload.single("avatar"), async (req, res, next) {
+  try {
+    await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toFile(__dirname + `/images/${req.file.originalname}`)
+         res.status(201).send('Image uploaded succesfully')
+  } catch (error) {
+    console.log(error)
+    res.status(400).send(error)
+  }
 });
 
 app.listen(PORT, () => {
