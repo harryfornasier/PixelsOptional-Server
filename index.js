a7ee3729d146eac61af06da41f7b133db902d183
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-11-09 11:20:40 +0000

    Add upload image functionality

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
+import multer from "multer";
 const app = express();
+const upload = multer({ dest: "uploads/" });
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -11,6 +13,12 @@ app.get("/test", (req, res) => {
 app.post("/test", (req, res) => {
   console.log(req.body);
   res.send({ msg: "Response to POST request to /" });
+});
+
+app.post("/image", upload.single("avatar"), function (req, res, next) {
+  res.send({ msg: "saved the image successfully" });
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
 });
 
 app.listen(PORT, () => {
