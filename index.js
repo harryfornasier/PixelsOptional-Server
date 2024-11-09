b45e02c53458364cf1e3dd734a56c9ad3316464d
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-11-09 13:01:45 +0000

    Add https

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express";
 import multer from "multer";
 import sharp from "sharp";
+import https from "https";
+import fs from "fs";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+const privateKey = fs.readFileSync("/home/Certificates/home_server.key", "utf8");
+const certifcate = fs.readFileSync("/home/Certificates/CSR.csr", "utf8");
+
+const credentials = { key: privateKey, cert: certifcate };
+
+const httpsServer = https.createServer(credentials, app);
 
 const upload = multer({
   limits: {
@@ -23,7 +32,7 @@ app.post("/image", upload.single("image"), async (req, res) => {
     await sharp(req.file.buffer)
       .resize({ width: 250, height: 250 })
       .png()
-      .toFile(`/images/${req.file.originalname}`);
+      .toFile(`./images/${req.file.originalname}`);
     res.status(201).send("Image uploaded succesfully");
   } catch (error) {
     console.log(error);
@@ -31,6 +40,8 @@ app.post("/image", upload.single("image"), async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log("App be listening");
-});
+httpsServer.listen(PORT);
+
+// app.listen(PORT, () => {
+//   console.log("App be listening");
+// });
