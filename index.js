ae3cc39b22581603785331394b026e1038c9e2b7
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-11-11 14:58:52 +0000

    Test making imageList null to start

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 const privateKey = fs.readFileSync("../Certificates/home_server.key", "utf8");
 const certifcate = fs.readFileSync("../Certificates/home_server.pem", "utf8");
 
-const imageList = [];
+const imageList = null;
 
 const credentials = { key: privateKey, cert: certifcate };
 
