7104f3017cbff48a1adad1deb2f5f26b0d237b7d
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-11-27 16:37:00 +0000

    Test new sharp values

--- routes/posts.js
+++ routes/posts.js
@@ -29,10 +29,10 @@ router.post("/", [authorise, upload.single("image")], async (req, res) => {
   const src = `https://harrisonfornasier.space/static/${newUuid}.jpg`;
   try {
     sharp(req.file.buffer)
-      .resize(1440, 1050, {
-        fit: "cover",
-      })
       .rotate()
+      .resize(1440, 1050, {
+        fit: "outside",
+      })
       .toFormat("jpg")
       .toFile(path);
     const imageData = {
