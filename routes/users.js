140e7fa2209490578f4931784d0ff934c07a97fe
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-11-27 11:54:33 +0000

    Add get profile by ID

--- routes/users.js
+++ routes/users.js
@@ -65,14 +65,32 @@ router.get("/profile", authorise, async (req, res) => {
     const posts = await knex("camera")
       .join("post", "camera.id", "post.camera_id")
       .where("post.user_id", req.token.id)
-      .join("user", "user.id", "post.user_id") // Join with the 'user' table
+      .join("user", "user.id", "post.user_id")
       .select("camera.*", "post.*", "user.name");
 
     delete user.password;
 
-    res.json({ user: user, posts: posts });
+    res.status(200).json({ user: user, posts: posts });
   } catch (error) {
     res.status(500).json({ message: "can't fetch user profile" });
+  }
+});
+
+router.get("/profile/:id", async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const user = await knex("user").where({ id: userId }).first();
+    const posts = await knex("camera")
+      .join("post", "camera.id", "post.camera_id")
+      .where("post.user_id", userId)
+      .join("user", "user.id", "post.user_id")
+      .select("camera.*", "post.*", "user.name");
+
+    delete user.password;
+    res.status(200).json({ user: user, posts: posts });
+  } catch (error) {
+    res.status(404).json({ msg: `Could not find user: ${error}` });
   }
 });
 
