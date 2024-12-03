74e05d0f02a97ced4c41456d405241d984b2d587
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-12-03 14:22:07 +0000

    Test keepmetadata

--- routes/posts.js
+++ routes/posts.js
@@ -36,10 +36,10 @@ router.post("/", authorise, async function (req, res) {
 
       try {
         sharp(req.file.buffer)
+          .keepMetadata()
           .resize(1440, 1050, {
             fit: "cover",
           })
-          .withMetadata()
           .toFormat("jpg")
           .toFile(path);
         const imageData = {
