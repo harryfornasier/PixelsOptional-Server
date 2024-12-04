b2b4f0fddd2b5c4324abad79c62f7a5af6a07948
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-12-04 18:44:45 +0000

    Test commentId

--- routes/comments.js
+++ routes/comments.js
@@ -47,6 +47,7 @@ router.get("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   const postId = req.params.id;
   const commentId = req.body.commentId;
+  console.log(commentId);
   try {
     const commentDelete = await knex("comment")
       .where("comment.id", commentId)
