e1bfc54cc87b15a0a1430617afd04c8ddb1a14d5
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-12-04 08:35:32 +0000

    Test new console log

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import posts from "./routes/posts.js";
 import comments from "./routes/comments.js";
 import users from "./routes/users.js";
 import "dotenv/config";
+import fs from "fs";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -22,4 +23,8 @@ app.use("/users", users);
 
 app.listen(PORT, () => {
   console.log("App be listening");
+
+  fs.readdirSync("./icons").forEach((file) => {
+    console.log(file);
+  });
 });
