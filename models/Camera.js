b79dbd74d83ffe42eb5665fb11bb524eb318d802
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-12-05 17:14:16 +0000

    Add variable

--- models/Camera.js
+++ models/Camera.js
@@ -8,7 +8,7 @@ export async function postCameraDb(newCamera) {
 }
 
 export async function getCamerasByProfileQuery(userId) {
-  knex("user_camera")
+  const cameras = knex("user_camera")
     .join("camera", "camera.id", "user_camera.camera_id")
     .where("user_camera.user_id", userId)
     .select("*");
