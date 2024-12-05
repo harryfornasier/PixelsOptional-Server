2e0263ffe33acb1144316d1747479ec4850c7fb2
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-12-05 16:54:53 +0000

    Remove obsolete query

--- models/Camera.js
+++ models/Camera.js
@@ -8,8 +8,6 @@ export async function postCameraDb(newCamera) {
 }
 
 export async function getCamerasByProfileQuery(userId) {
-  const user = await knex("user").where("user.id", 1).leftJoin();
-
   const cameras = knex("camera")
     .join("user_camera", "camera.id", "user_camera.camera_id")
     .where("user_camera.user_id", userId)
