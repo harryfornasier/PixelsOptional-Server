3b43192e4e2be862b3f8f0637f3419db06273373
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-12-05 14:26:57 +0000

    Update object

--- controllers/postCameraController.js
+++ controllers/postCameraController.js
@@ -4,9 +4,9 @@ import { addCameraUserDb } from "../models/User.js";
 export async function postCamera(req, res) {
   const userId = req.token.id;
   const camera = {
-    camera_model: req.body.cameraModel,
-    camera_year: req.body.cameraYear,
-    camera_brand: req.body.cameraBrand,
+    model: req.body.cameraModel,
+    year: req.body.cameraYear,
+    brand: req.body.cameraBrand,
   };
 
   const newCamera = await postCameraDb(camera);
