6daa50ee5801ebffac1c4fec90fa0842e9b027c2
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-12-05 11:22:09 +0000

    Test photo models

---
+++ controllers/postController.js
@@ -0,0 +1,110 @@
+import { v4 as uuidv4 } from "uuid";
+import sharp from "sharp";
+import multer from "multer";
+import {
+  getPostByIdDb,
+  postImageDb,
+  getPostsDb,
+  deletePostDb,
+  likePostDb,
+} from "../models/Post.js";
+
+const upload = multer({
+  limits: {
+    fileSize: 5000000,
+  },
+
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|JPG)$/)) {
+      return cb(new Error("Please upload a valid image file"));
+    }
+    cb(undefined, true);
+  },
+}).single("image");
+
+export async function postImage(req, res) {
+  upload(req, res, async function (err) {
+    if (err instanceof multer.MulterError) {
+      res.status(413).json({ msg: "Image too large" });
+    } else if (err) {
+      res.status(500).json({ msg: "Unknown error" });
+    } else {
+      const newUuid = uuidv4();
+      let landscape = false;
+      const path = `./images/${newUuid}.jpg`;
+      const src = `https://harrisonfornasier.uk/static/${newUuid}.jpg`;
+
+      try {
+        const image = await sharp(req.file.buffer)
+          .keepMetadata()
+          .toFormat("jpg")
+          .toFile(path);
+
+        if (image.height > image.width) {
+          landscape = false;
+        } else {
+          landscape = true;
+        }
+        const imageData = {
+          user_id: req.token.id,
+          title: req.body.title,
+          content: "",
+          image_url: src,
+          camera_id: 1,
+          orientation: landscape,
+        };
+        const newPost = await postImageDb();
+        res.status(201).send({ msg: "Image uploaded succesfully", newPost });
+      } catch (error) {
+        console.log(error);
+        res.status(400).send(error);
+      }
+    }
+  });
+}
+
+export async function getPosts(req, res) {
+  const offset = parseInt(req.query.page) * 21 - 21;
+  const userId = req.query.userId;
+  try {
+    if (!userId) {
+      const posts = await getPostsDb(userId, offset);
+      res.status(200).json(posts);
+    }
+  } catch (error) {
+    res.status(500).json({ mesesage: `Error fetching from database: ${error}` });
+  }
+}
+
+export async function getPostById(req, res) {
+  const postId = req.params.id;
+  try {
+    const post = await getPostByIdDb(postId);
+    res.status(200).json({ msg: "Found post succesfully", post });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+}
+
+export async function likePost(req, res) {
+  const postId = parseInt(req.params.id);
+  const givingUserId = parseInt(req.token.id);
+  const receivingUser = req.body.foreignUser;
+  try {
+    const likePhoto = await likePostDb(postId, givingUserId, receivingUser);
+
+    res.status(200).json({ msg: "Liked the image" });
+  } catch (error) {
+    res.status(500).json({ msg: "Unknown error", error });
+  }
+}
+
+export async function deletePost(req, res) {
+  const user = await deletePost(postId);
+
+  if (!user.admin) {
+    res.status(403).json({ msg: "You're not allowed to delete posts" });
+  } else {
+    res.status(204).json({ msg: "Deleted post" });
+  }
+}
---
+++ models/Post.js
@@ -0,0 +1,140 @@
+import initKnex from "knex";
+import knexConfig from "../knexfile.js";
+
+const knex = initKnex(knexConfig);
+
+export async function postImageDb() {
+  const newPost = await knex("post").insert(imageData);
+  return newPost;
+}
+
+export async function getPostsDb(userId, offset) {
+  if (!userId) {
+    const posts = await knex("camera")
+      .join("post", "camera.id", "post.camera_id")
+      .join("user", "user.id", "post.user_id")
+      .leftJoin("post_like", "post.id", "post_like.post_id")
+      .leftJoin("comment", "post.id", "comment.post_id")
+      .select(
+        "camera.*",
+        "post.*",
+        "user.name",
+        "user.icon_url",
+        knex.raw("COUNT(post_like.post_id) as like_count"),
+        knex.raw("COUNT(DISTINCT comment.id) as comment_count")
+      )
+      .groupBy("camera.id", "post.id", "user.id")
+      .limit(21)
+      .offset(offset);
+    return posts;
+  } else {
+    const posts = await knex("camera")
+      .orderBy("created_at", "desc")
+      .join("post", "camera.id", "post.camera_id")
+      .join("user", "user.id", "post.user_id")
+      .leftJoin("post_like", "post.id", "post_like.post_id")
+      .leftJoin("comment", "post.id", "comment.post_id")
+      .select(
+        "camera.*",
+        "post.*",
+        "user.name",
+        "user.icon_url",
+        knex.raw("COUNT(post_like.post_id) as like_count"),
+        knex.raw("COUNT(DISTINCT comment.id) as comment_count"),
+        knex.raw(`
+    EXISTS (
+      SELECT 1
+      FROM post_like
+      WHERE post_like.post_id = post.id
+        AND post_like.user_id = ${userId}
+    ) AS user_liked
+  `)
+      )
+      .groupBy("camera.id", "post.id", "user.id")
+      .limit(21)
+      .offset(offset);
+    return posts;
+  }
+}
+
+export async function getPostByIdDb(postId) {
+  const post = await knex("post")
+    .leftJoin("camera", "post.camera_id", "camera.id")
+    .leftJoin("post_like", "post.id", "post_like.post_id")
+    .leftJoin("comment", "post.id", "comment.post_id")
+    .select(
+      "post.id as post_id",
+      "post.created_at",
+      "post.updated_at",
+      "post.user_id",
+      "post.title",
+      "post.comment_count",
+      "post.content",
+      "post.image_url",
+      "post.orientation",
+      "camera.id as camera_id",
+      "camera_model as camera_model",
+      "camera_year as camera_year",
+      "camera_brand as camera_brand",
+      knex.raw("COUNT(DISTINCT post_like.user_id) as like_count"),
+      knex.raw("COUNT(DISTINCT comment.id) as comment_count")
+    )
+    .where("post.id", postId)
+    .groupBy(
+      "post.id",
+      "post.created_at",
+      "post.updated_at",
+      "post.user_id",
+      "post.title",
+      "post.comment_count",
+      "post.content",
+      "post.image_url",
+      "post.orientation",
+      "camera.id",
+      "camera_model",
+      "camera_year",
+      "camera_brand"
+    );
+  return post;
+}
+
+export async function likePostDb(postId, givingUserId, receivingUser) {
+  const alreadyLiked = await knex("post_like")
+    .where({ user_id: givingUserId, post_id: postId })
+    .first();
+
+  if (alreadyLiked) {
+    //undo
+  } else {
+    const givingUser = await knex("user").where("user.id", givingUserId).first();
+
+    if (givingUser.pot < 1) {
+      return false;
+      res.status(403).json({ msg: "You don't have enough likes in your pot" });
+    } else if (givingUser.id === receivingUser) {
+      return false;
+      res.status(403).json({ msg: "You can't like your own posts" });
+    } else {
+      const user = await knex("user").where("id", givingUserId).first();
+      const post = await knex("post").where("id", postId).first();
+
+      //
+      const like = await knex("post_like").insert({
+        user_id: givingUserId,
+        post_id: postId,
+      });
+
+      //
+      const givingUserDecrease = await knex("user")
+        .increment("pot", -1)
+        .where("user.id", givingUserId);
+
+      return true;
+    }
+  }
+}
+
+export async function deletePostDb(postId) {
+  const user = await knex("user").where("id", req.token.id).first();
+  return user;
+}
