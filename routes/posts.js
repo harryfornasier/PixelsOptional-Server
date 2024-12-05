4966f151b32a9e759639c3f99616530c69c233f3
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-12-05 11:24:08 +0000

    Update router

--- routes/posts.js
+++ routes/posts.js
@@ -1,29 +1,22 @@
+import {
+  deletePost,
+  getPostById,
+  getPosts,
+  likePost,
+  postImage,
+} from "../controllers/postController.js";
 import express from "express";
 import authorise from "./middleware/auth.js";
 const router = express.Router();
 
-router.post("/", authorise, async function (req, res) {});
+router.post("/", authorise, postImage);
 
-router.get("/", async (req, res) => {});
+router.get("/", getPosts);
 
-router.get("/:id", async (req, res) => {});
+router.get("/:id", getPostById);
 
-router.patch("/:id", authorise, async (req, res) => {});
+router.patch("/:id", authorise, likePost);
 
-router.delete("/:id", authorise, async (req, res) => {
-  const postId = req.params.id;
-  try {
-    const user = await knex("user").where("id", req.token.id).first();
-
-    if (!user.admin) {
-      res.status(403).json({ msg: "You're not allowed to delete posts" });
-    } else {
-      const deletePost = await knex("post").where("id", postId).del();
-      res.status(204).json({ msg: "Deleted post" });
-    }
-  } catch (error) {
-    res.status(500).json({ msg: "Unknown error occured" + error });
-  }
-});
+router.delete("/:id", authorise, deletePost);
 
 export default router;
