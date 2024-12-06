8085febcc81f503bed28673650345bf7da66a6a4
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-12-06 11:48:44 +0000

    Update post camera validation

--- controllers/postCameraController.js
+++ controllers/postCameraController.js
@@ -3,15 +3,28 @@ import { addCameraUserDb } from "../models/User.js";
 
 export async function postCamera(req, res) {
   const userId = req.token.id;
+
   const camera = {
     model: req.body.cameraModel,
     year: req.body.cameraYear,
     brand: req.body.cameraBrand,
   };
 
-  const newCamera = await postCameraDb(camera);
-  await addCameraUserDb(userId, newCamera);
-  res.status(201).send(newCamera);
+  try {
+    if (!camera.model) {
+      res.status(400).json({ msg: "No camera model included" });
+    } else if (!camera.brand) {
+      res.status(400).json({ msg: "No camera brand included" });
+    } else if (!camera.year) {
+      res.status(400).json({ msg: "No camera year included" });
+    } else {
+      const newCamera = await postCameraDb(camera);
+      await addCameraUserDb(userId, newCamera);
+      res.status(201).json({ msg: "Posted new camera", newCamera });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: "Unknown error", error });
+  }
 }
 
 export async function getCamerasByUser(req, res) {
